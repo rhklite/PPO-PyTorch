9df433122fbed7a1d50f46e4ca9e7f41b20a6209
richard <hanhu@mie.utoronto.ca>
2019-11-12 00:06:23 -0500

    added hogwild example

---
+++ multi_processing_test/process_pytorch_share_memory.py
@@ -0,0 +1,28 @@
+import torch
+import torch.multiprocessing as mp
+import os
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+mp.set_start_method("spawn", True)
+
+
+def add_tensors(tensor):
+    tensor = tensor + 1
+    print("process ID {}, Tensor: {}, shared: {}".format(
+        os.getpid(), tensor, tensor.is_shared()))
+
+
+if __name__ == "__main__":
+    tensor = torch.zeros(5).to(device)
+    tensor.storage().share_memory_()
+    print()
+    processes = []
+    for _ in range(5):
+        p = mp.Process(target=add_tensors, args=(tensor,))
+        p.start()
+        processes.append(p)
+    for p in processes:
+        p.join()
+
+    print("main process {}, is shared {}".format(
+        tensor, tensor.is_shared()))
